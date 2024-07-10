3969468dc5eb2e335afc8fa4d4df89fdab41fd42
Hashim Aziz Muhammad <hashimazizm@gmail.com>
2024-07-10 16:24:11 +0200

    chnages

--- Back End Development/Week 6 - Programming Paradigms/bank_balance_scripts/pythonProject/bank_account.py
+++ Back End Development/Week 6 - Programming Paradigms/bank_balance_scripts/pythonProject/bank_account.py
@@ -13,4 +13,4 @@ class BankAccount:
         return False
 
     def display_balance(self):
-        print(f"Current balance: ${self.account_balance:.2f}")
+        print(f"Current balance:${self.account_balance:.2f}")
