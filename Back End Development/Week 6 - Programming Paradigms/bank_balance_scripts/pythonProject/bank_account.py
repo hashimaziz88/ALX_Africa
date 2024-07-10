42ba0eb32947706377b5b8479289bd80a07291e1
Hashim Aziz Muhammad <hashimazizm@gmail.com>
2024-07-10 16:17:25 +0200

    commits bank_account

--- Back End Development/Week 6 - Programming Paradigms/bank_balance_scripts/pythonProject/bank_account.py
+++ Back End Development/Week 6 - Programming Paradigms/bank_balance_scripts/pythonProject/bank_account.py
@@ -1,12 +1,16 @@
 class BankAccount:
-    def __init__(self, account_balance):
-        self.account_balance = account_balance
+    def __init__(self, initial_balance=0):
+        self.account_balance = initial_balance
 
-        def deposit(amount):
-            pass
+    def deposit(self, amount):
+        if amount > 0:
+            self.account_balance += amount
 
-        def withdraw(amount):
-            pass
+    def withdraw(self, amount):
+        if 0 < amount <= self.account_balance:
+            self.account_balance -= amount
+            return True
+        return False
 
-        def display_balance():
-            pass
+    def display_balance(self):
+        print(f"Current balance: ${self.account_balance:.2f}")
